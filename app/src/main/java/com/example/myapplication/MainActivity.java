f6a0076d4b3af4a8e345c0af96d3685bc04e4596
hasanmurad4919 <hasanmurad4919@gmail.com>
2022-08-02 16:48:55 +0600

    change

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,25 +2,65 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.Calendar;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, SensorEventListener {
     Button SetButton;
     Button ScheduleButton;
+    private SensorManager sensorManager;
+    private Sensor Temperature;
+    float temp;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE)!=null){
+            Temperature = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        }
+        else{
+            Toast.makeText(this, temp+ "No temperatue sensor available", Toast.LENGTH_LONG).show();
+        }
+
+        startSensorAlert();
+
+
         SetButton= (Button) findViewById(R.id.SetAlarmButton);
         ScheduleButton=(Button) findViewById(R.id.ScheduleAlarmButton);
         SetButton.setOnClickListener(this);
         ScheduleButton.setOnClickListener(this);
+
+
+    }
+    public void startSensorAlert(){
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        Intent intent = new Intent(this, SensorReceiver.class);
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this.getApplicationContext(), 234324243, intent, 0);
+
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()
+                , 1000 * 60 * 10, pendingIntent);
 
     }
 
@@ -36,4 +76,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         }
     }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+         temp = sensorEvent.values[0];
+         SensorReceiver.temp=temp;
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, Temperature, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        // Be sure to unregister the sensor when the activity pauses.
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+
+
 }
--- app/src/main/java/com/example/myapplication/SensorReceiver.java
+++ app/src/main/java/com/example/myapplication/SensorReceiver.java
@@ -1,2 +1,33 @@
-package com.example.myapplication;public class SensorReceiver {
+package com.example.myapplication;
+
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import android.media.MediaDescription;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Date;
+
+
+public class SensorReceiver extends BroadcastReceiver {
+
+    public static float temp;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+
+        Toast toast = Toast.makeText(context, temp+ "°C", Toast.LENGTH_LONG);
+        toast.show();
+
+
+    }
+
 }
